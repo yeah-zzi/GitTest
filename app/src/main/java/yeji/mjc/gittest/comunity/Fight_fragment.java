3aba60c316e21c3d75dc548427fd11d72cd67be7
hyerim23 <119495992+hyerim23@users.noreply.github.com>
2023-05-16 13:43:43 +0900

    fight fragment 수정

--- app/src/main/java/yeji/mjc/gittest/comunity/Fight_fragment.java
+++ app/src/main/java/yeji/mjc/gittest/comunity/Fight_fragment.java
@@ -1,10 +1,13 @@
 package yeji.mjc.gittest.comunity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import yeji.mjc.gittest.R;
@@ -19,10 +22,11 @@ public class Fight_fragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.food_fight, container, false);
 
+
         return view;
     }
 
 
 
-//test
+// test
 }
