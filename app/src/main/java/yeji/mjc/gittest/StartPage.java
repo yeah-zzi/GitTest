6f7a888bc061c39c467be9254e4ac1a2a22e5a89
hyerim23 <119495992+hyerim23@users.noreply.github.com>
2023-05-23 16:50:38 +0900

    냉장고 firebase

--- app/src/main/java/yeji/mjc/gittest/StartPage.java
+++ app/src/main/java/yeji/mjc/gittest/StartPage.java
@@ -1,7 +1,12 @@
 package yeji.mjc.gittest;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -10,6 +15,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import yeji.mjc.gittest.FoodSearch.FridgePlus;
 import yeji.mjc.gittest.register.Register;
@@ -25,6 +33,25 @@ public class StartPage extends AppCompatActivity {
         setContentView(R.layout.startpage);
 
         loginBtn = findViewById(R.id.loginButton);
+
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (packageInfo == null)
+            Log.e("KeyHash", "KeyHash:null");
+
+        for (Signature signature : packageInfo.signatures) {
+            try {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            } catch (NoSuchAlgorithmException e) {
+                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+            }
+        }
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
