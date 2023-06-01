04b180b82b101d3e030043dc73bccddc9d7d8ad0
leesujin0319 <sujin3194@gmail.com>
2023-06-02 02:12:03 +0900

    바코드 완료, 장바구니 추가버튼, 마이페이지 알러지 수정중

---
+++ app/src/main/java/yeji/mjc/gittest/mypage/MyPageAllergyItem.java
@@ -0,0 +1,19 @@
+package yeji.mjc.gittest.mypage;
+
+public class MyPageAllergyItem {
+
+    int allergy_img;
+    public MyPageAllergyItem(){}
+
+    public MyPageAllergyItem(int allergy_img) {
+        this.allergy_img = allergy_img;
+    }
+
+    public int getAllergy_img() {
+        return allergy_img;
+    }
+
+    public void setAllergy_img(int allergy_img) {
+        this.allergy_img = allergy_img;
+    }
+}
