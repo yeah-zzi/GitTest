383c7a6ee12e72a7b1d265bf9e8781d70970c325
hyerim23 <119495992+hyerim23@users.noreply.github.com>
2023-06-04 15:41:47 +0900

    아이템 삭제 오류 수정/정렬 기능 구현중

--- app/src/main/java/yeji/mjc/gittest/frige/Fridge_Adapter.java
+++ app/src/main/java/yeji/mjc/gittest/frige/Fridge_Adapter.java
@@ -8,14 +8,18 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
+import yeji.mjc.gittest.FoodItem;
 import yeji.mjc.gittest.R;
 
 public class Fridge_Adapter extends RecyclerView.Adapter<Fridge_recycle_holder> {
 
-    //Context context;
+    Fridge_Adapter fridge_adapter;
     ArrayList<Fridge_Item> fridgeItems;
     int count = 0;
+
 
     public Fridge_Adapter(ArrayList<Fridge_Item> fridgeItems) {
         this.fridgeItems = fridgeItems;
@@ -26,21 +30,12 @@ public class Fridge_Adapter extends RecyclerView.Adapter<Fridge_recycle_holder> 
         return new Fridge_recycle_holder(v);
     }
 
-    //롱클릭한 Item을 삭제할 때 사용할 메소드 구현
-    public void removeItem(int itemPosition) {
-        //removeItem() 메소드 내에서 position이 유효한 범위인지를 검사
-        if (itemPosition >= 0 && itemPosition < fridgeItems.size()) {
-            fridgeItems.remove(itemPosition);
-            notifyItemRemoved(itemPosition);
-        }
-
-    }
-
     @Override
     public void onBindViewHolder(@NonNull Fridge_recycle_holder holder, int position) {
 
         int itemPosition = holder.getAdapterPosition();
         //아이템의 위치를 동적으로 가져옴.
+        fridge_adapter = this; // fridge_adapter 객체 초기화
 
         holder.food_img.setImageResource(fridgeItems.get(position).getFood_img());
         holder.food_name.setText(fridgeItems.get(position).getFood_name());
@@ -79,11 +74,27 @@ public class Fridge_Adapter extends RecyclerView.Adapter<Fridge_recycle_holder> 
                 holder.btn_cart.setVisibility(View.VISIBLE);
                 holder.btn_delete.setVisibility(View.VISIBLE);
 
+                //롱클릭의 후속 리스너..
                 holder.btn_delete.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         if (itemPosition != RecyclerView.NO_POSITION) {
-                            removeItem(itemPosition);
+
+                            //해당 아이템 삭제
+                            fridgeItems.remove(itemPosition);
+                            fridge_adapter.removeItem(itemPosition);
+                            fridge_adapter.notifyItemRemoved(itemPosition);
+                            fridge_adapter.notifyDataSetChanged();
+
+                            //아이템을 삭제했다면 나머지 식재료들은 원상태로 복구
+                            holder.close.setVisibility(View.GONE);
+                            holder.food_count.setVisibility(View.VISIBLE);
+                            holder.count_plus.setVisibility(View.VISIBLE);
+                            holder.count_minus.setVisibility(View.VISIBLE);
+                            holder.food_date.setVisibility(View.VISIBLE);
+                            holder.btn_cart.setVisibility(View.GONE);
+                            holder.btn_delete.setVisibility(View.GONE);
+
                         }
                     }
                 });
@@ -109,6 +120,41 @@ public class Fridge_Adapter extends RecyclerView.Adapter<Fridge_recycle_holder> 
         });
 
 
+    }
+
+
+    //정렬 기준을 나타내는 enum
+    public enum SortType {
+        NAME_ASCENDING, //Food_name을 기준으로 오름차순
+        // 추가적인 정렬 기준은 이곳에 추가
+    }
+
+    private SortType currentSortType = SortType.NAME_ASCENDING; //현재 정렬 기준
+
+    // 아이템 정렬 메서드
+    private void sortItems() {
+        switch (currentSortType) {
+            case NAME_ASCENDING:
+                Collections.sort(fridgeItems, new Comparator<Fridge_Item>() {
+                    @Override
+                    public int compare(Fridge_Item item1, Fridge_Item item2) {
+                        return item1.getFood_name().compareTo(item2.getFood_name());
+                    }
+                });
+                break;
+            // 추가적인 정렬 기준에 따라 case를 추가
+        }
+    }
+
+    // 현재 정렬 기준을 설정하는 메서드
+    public void setSortType(SortType sortType) {
+        currentSortType = sortType;
+        sortItems();
+        notifyDataSetChanged(); //정렬 결과를 반영.
+    }
+
+
+    private void removeItem(int position) {
     }
 
     @Override
