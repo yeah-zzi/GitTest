fa3f6df9f64306fae20aeb2f91fa1c2c9e72e534
82109 <kimgeulim>
2023-06-06 14:57:16 +0900

    게시글 작성시 파이어베이스 유저부분에 작성한 게시글의 코드가 저장됨, 생활정보 누르면 세부 게시글 들어가는거 구현완료

--- app/src/main/java/yeji/mjc/gittest/comunity/Life_Fragment.java
+++ app/src/main/java/yeji/mjc/gittest/comunity/Life_Fragment.java
@@ -1,9 +1,11 @@
 package yeji.mjc.gittest.comunity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -16,11 +18,13 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.w3c.dom.Comment;
+
 import java.util.ArrayList;
 
 import yeji.mjc.gittest.R;
 
-public class Life_Fragment extends Fragment {
+public class Life_Fragment extends Fragment implements SelectListener{
 
     private ArrayList<TipItem> list = new ArrayList<TipItem>(); //리사이클러뷰에 추가할 아이템 리스트
     private LifePost_Adapter adapter; // 어뎁터
@@ -36,7 +40,7 @@ public class Life_Fragment extends Fragment {
 
         recyclerView = (RecyclerView) view.findViewById(R.id.life_recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        adapter = new LifePost_Adapter(list);
+        adapter = new LifePost_Adapter(list,this);
         recyclerView.setAdapter(adapter);
 
         //파이어베이스에 저장되어 있는 회원의 알러지 정보를 받아 해당하는 알러지를 recyclerview로 나타낸다
@@ -63,5 +67,20 @@ public class Life_Fragment extends Fragment {
     }
 
 
+    @Override
+    public void onItemClicked(TipItem tip_fragment) {
+        Intent comIntent = new Intent(getActivity(), LifeCommnet.class);
+        String code = tip_fragment.com_code;
+        comIntent.putExtra("커뮤니티 코드",code);
+        comIntent.putExtra("작성자",tip_fragment.getWriter());
+        comIntent.putExtra("내용",tip_fragment.getContent());
+        comIntent.putExtra("이미지",tip_fragment.getPost_img());
+        comIntent.putExtra("제목",tip_fragment.getTitle());
+        comIntent.putExtra("좋아요",tip_fragment.getLike());
+        comIntent.putExtra("댓글수",tip_fragment.getComment_count());
+        comIntent.putExtra("작성자이미지",tip_fragment.getWriter_img());
+        comIntent.putExtra("작성날짜",tip_fragment.getDate());
+        startActivity(comIntent);
+    }
 }
 
